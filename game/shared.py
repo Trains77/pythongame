0497edb0ebb3a1bf83623d1660fbd42d0046c460
Trains77 <trains77videos123@gmail.com>
2021-10-31 13:03:52 -0400

    Tweaks to setting discriptions

--- game/shared.py
+++ game/shared.py
@@ -11,24 +11,23 @@ block_color = (0, 0, 0) # Block Color
 info_color = (150, 150, 150) # Info square color
 dialog_color = (255, 255, 255) # Dialog box color
 background_color = (128, 0, 128) # The background color of the game
-show_debug = False
+show_debug = False # Enable debugging messages
 
 # Game Settings
-disable_background = True # Set whether the game loads the background file
 enable_os_restrictions = 0 # Whether to enable strict os checking
 credits = 1 # Whether to display credits at startup
-GameName = "Another Python Game" # The window name of the game
-song = 'Audio/Music/music1.wav' # Where the game's music file is stored
-enable_music = 1 # Whether or not the game should play music
+GameName = "Another Python Game" # The name of the game
+song = 'Audio/Music/music1.wav' # Path to music file
+enable_music = 1 # Whether or not music is enabled
 gameIcon = pygame.image.load('Textures/Icons/game.png') # Path to game's window icon
 
 # WARNING: Changing these settings will cause issues
 enable_os_restrictions = 0 # Whether to enable strict os checking
 size = [500, 500] # The size of the screen
-square_size = 20 # How big the default square hitbox is
-game_border1 = 480 # Maximum cordinates player can go
-game_border2 = 0 # Minimum cordinates playerr can go
+square_size = 20 # How big the default hitbox is
+game_border1 = 480 # Maximum cordinates player can be
+game_border2 = 0 # Minimum cordinates playerr can be
 speed = 10 # How fast the player moves
-enable_program = True # Enable or Disable the program
-item_size = 10 # Item hitbox size
-fps = 10 # The game's FPS
+enable_program = True # Enables or Disables the program
+item_size = 10 # The size of an item's hitbox
+fps = 10 # The FPS of the game
