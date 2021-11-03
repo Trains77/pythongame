7f993ec50deae2dbad05500a1c2f29f80a5df6c0
Trains77 <trains77videos123@gmail.com>
2021-11-03 10:45:46 -0400

    Move notice to its own function

--- game/game.py
+++ game/game.py
@@ -48,12 +48,13 @@ def createdialog(speaker, text):
     img1 = font1.render(speaker + ":", True, BLACK)
     img2 = font1.render(text, True, BLACK)
     img3 = font1.render("Press 'z' to continue", True, BLACK)
-    img4 = pygame.image.load(characters_path + "speaking.png")
     screen.blit(img1, (15, 360))
     screen.blit(img2, (30, 390))
     screen.blit(img3, (355, 470))
-    screen.blit(img4, (infox, infoy - 20))
 
+def create_notice(posx, posy):
+    img4 = pygame.image.load(characters_path + "speaking.png")
+    screen.blit(img4, ([posx, posy - 20))
 def image_display(surface, filename, xy):
     img = pygame.image.load(filename)
     surface.blit(img, xy)
